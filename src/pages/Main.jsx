24d4f9b63b5df78340bcf3d91ef91cc6d14e92ff
RebikHub <rebikovlk@mail.ru>
2022-04-29 16:28:59 +0700

    main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/main.css';
 
+import { reviews } from '../reviews';
+
 export default function Main() {
   return (
     <main className='main'>
@@ -26,12 +28,50 @@ export default function Main() {
       </div>
 
       <div className='main-how'>
-
+        <div className='how-title'>
+          <h4 className='how-title-text'>как это работает</h4>
+          <button className='how-title-btn' type='button'>Узнать больше</button>
+        </div>
+        <div className='how-content'>
+          <div className='how-content-item'>
+            <div className='how-image-first'></div>
+            <p className='how-content-text'>Удобный заказ на сайте</p>
+          </div>
+          <div className='how-content-item'>
+            <div className='how-image-second'></div>
+            <p className='how-content-text'>Нет необходимости ехать в офис</p>
+          </div>
+          <div className='how-content-item'>
+            <div className='how-image-third'></div>
+            <p className='how-content-text'>Огромный выбор направлений</p>
+          </div>
+        </div>
       </div>
 
       <div className='main-reviews'>
-
+        <div className='reviews-title'>отзывы</div>
+        <div className='reviews'>
+          {reviews.map((el, i) => (
+            <div className='review' key={i}>
+              <img className='review-image' src={el.image} alt={el.name}/>
+              <div className='review-content'>
+                <h5 className='review-name'>{el.name}</h5>
+                <p className='review-text'>
+                  {el.content}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className='reviews-carousel'>
+          <div className='carousel-dots active-dot'></div>
+          <div className='carousel-dots'></div>
+          <div className='carousel-dots'></div>
+          <div className='carousel-dots'></div>
+          <div className='carousel-dots'></div>
+        </div>
       </div>
+
     </main>
   )
 }
---
+++ src/reviews.js
@@ -0,0 +1,12 @@
+export const reviews = [
+  {
+    name: 'Екатерина Вальнова',
+    content: 'Доброжелательные подсказки на всех этапах помогут правильно заполнить поля и без затруднений купить авиа или ж/д билет, даже если вы заказываете онлайн билет впервые.',
+    image: 'https://s3-alpha-sig.figma.com/img/b9b8/b89d/2fec5f0f798b8787930c80dd5d6472af?Expires=1652054400&Signature=Dw6suQFkutXRbIzduIclJunIIIUCikgl-ur7WKMoBVroPrAJI~VB87gf5HtoZVKPfylfdAJOS6GxiPc-F4BHLUlGFlhhZi8zxOtm6vlUzU-kvhRa444S9jcT6-g2zbVt6FWR5Aa3qN-eCs0Xh3wjg1sm2Vdplsq9uTPIHRs16~A0YLB6ShoT--~uQ0G86dHgiJPiQS73KtFrYSc4SylP8ZEc69kCCLZKf9Nda0FrIoweC1YehK~e8xoZT3JyeFeNN89tjL1GVAZTQcqJB-K1INLh~IDHCTqg0~doeGVtQfrAUeMlmh-dOxe6PsSe97-N2g2xCJnBm1tsPqgefLYZ6A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+  },
+  {
+    name: 'Евгений Стрыкало',
+    content: 'СМС-сопровождение до посадки Сразу после оплаты ж/д билетов и за 3 часа до отправления мы пришлем вам СМС-напоминание о поездке.',
+    image: 'https://s3-alpha-sig.figma.com/img/9a94/671a/adb42ec0d354460adb5d422bc34dc958?Expires=1652054400&Signature=bkWIBc83EjKT3lQAkdXrzhi4ufRgTDoongyOE1pljyIZ-F5oWuBWC61BQWteKfU49-mh8JV1nXGnSWr0prwPGagwS1vvihIlxVZQEQoQqbYKWNmLYv2vNKLUFh2l-rx6GG~vTC8s4JLHxtZSrlgHvM-Ew1nRK1tY3krkk5F0-mIRHXWtcwdIinVijzwdKHxtTwi~iqSXF5jj-~lEJtibjVcSIsI2xRip5oq1nObUDs2lNAr0p5hkjkXaVywbEewfabQHKeNFsCP0esm3vGttRf2sPPdKY92JVBIK0Ci6qM665HcSMF-31EY9ZFXsXIa3WUMevRneRqsUlcukazqQlg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+  }
+]
