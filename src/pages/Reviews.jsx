eada6b8f138477b8711f59ff0de463d1c589a945
RebikHub <rebikovlk@mail.ru>
2022-05-21 18:41:35 +0700

    carousel

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,35 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { reviews } from '../reviews';
+import '../styles/carousel.css';
 
 export default function Reviews() {
-  const arrF = [{ s: "one" }, { s: "two" }, { s: "three" }];
+  // const arr = [{ s: "one" }, { s: "two" }, { s: "three" }];
   const [array, setArray] = useState(reviews);
-  console.log(array);
-  function carousel(arr) {
-    console.log(arr);
-    const firstEl = arr.shift();
-    console.log(firstEl);
-    console.log(arr);
-    const resultArr = arr.push(firstEl);
-    console.log(resultArr);
-    return setArray(resultArr);
-  };
+  const [num, setNum] = useState(0);
 
-  useEffect(() => {
-    carousel(array)
-  }, [])
+  // function carousel(arr) {
+  //   const changeArray = arr;
+  //   const firstEl = changeArray[0];
+  //   changeArray.splice(0,1);
+  //   changeArray.push(firstEl)
+  //   setArray(changeArray);
+  // };
 
+  // function counter(arg, arr) {
+  //   carousel(arr);
+  //   setNum(arg + 1);
+  // }
+  
   // useEffect(() => {
-  //   const timer = setTimeout(() => carousel(array), 2 * 1000);
-  //   return clearTimeout(timer);
-  // }, [array]);
+  //   const carouselInterval = setTimeout(() => counter(num, array), 2 * 1000);
+  //   return () => clearTimeout(carouselInterval);
+  // });
 
   return (
     <div id='main-reviews' className='main-reviews'>
       <div className='reviews-title'>отзывы</div>
       <div className='reviews'>
         {array.map((el, i) => (
-          <div className='review' key={i}>
+          <div className='review' key={el.name + i}>
             <img className='review-image' src={el.image} alt={el.name}/>
             <div className='review-content'>
               <h5 className='review-name'>{el.name}</h5>
