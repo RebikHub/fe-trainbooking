b95c562a2f68f3476a8009003b7eee709a1fcd7c
RebikHub <rebikovlk@mail.ru>
2022-06-02 16:35:44 +0700

    train route

--- src/components/ListRoutes.jsx
+++ src/components/ListRoutes.jsx
@@ -1,40 +1,77 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/list-routes.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { currentStepOne } from '../store/sliceProgressLine';
 import TrainRoute from './TrainRoute';
 
 export default function ListRoutes() {
-  const { loading } = useSelector((state) => state.sliceGetRoute);
+  const { loading, route } = useSelector((state) => state.sliceGetRoute);
   const dispatch = useDispatch();
-  const arr = [1, 2, 3, 5, 6, 7, 8, 9, 10];
-  const pages = []
-
+  const [none, setNone] = useState('none');
+  const [select, setSelect] = useState('времени');
+  console.log(route);
+  const [pages, setPages] = useState([]);
 
   useEffect(() => {
     if (!loading) {
       dispatch(currentStepOne());
     };
-    for (let i = 0; i < arr.length / 5; i += 1) {
-      pages.push(i);
+    
+    if (route.items) {
+      setPages([]);
+      for (let i = 0; i < (route.items.length / 5); i += 1) {
+        setPages([...pages, i]);
+      };
     };
-  }, []);
 
+  }, [route]);
+
+  function getSort() {
+    if (none === 'none') {
+      setNone('list-routes-sort-select');
+    } else {
+      setNone('none');
+    }
+  };
+
+  function getSelect(ev) {
+    setSelect(ev.target.outerText);
+    setNone('none');
+  };
+
+  function choicePage(ev) {
+    const elements = document.querySelectorAll('.list-routes-page');
+    for (let i of elements) {
+      if (i.classList.contains('choice-page')) {
+        i.classList.remove('choice-page');
+      };
+    };
+    
+    if (ev.target.className === 'list-routes-page') {
+      ev.target.className += ' choice-page';
+    };
+  };
+  // if (!route.items && !route.items.length) {
+  // if (!route.items) {
+  //   return <div className='no-routes'>Ничего не найдено!</div>
+  // };
 
   return (
     <div className='list-routes'>
       <header className='header-list-routes'>
         <div className='list-routes-found'>
           <p>найдено </p>
-          <span>5</span>
+          <span>{route.items ? route.items.length : ''}</span>
         </div>
         <div className='list-routes-sort'>
-          <p>сортировать по: </p>
-          <select name="select" className='list-routes-sort-select'>
-            <option value="time" selected>времени</option>
-            <option value="price">стоимости</option>
-            <option value="duration">длительности</option>
-          </select>
+          <p>сортировать по: 
+            <span className='sort-selected' onClick={getSort}>{select}</span>
+          </p>
+          <div className={none}>
+            <div className='select-time' onClick={getSelect}>времени</div>
+            <div className='select-price' onClick={getSelect}>стоимости</div>
+            <div className='select-duration' onClick={getSelect}>длительности</div>
+          </div>
         </div>
         <div className='list-routes-show'>
           <p>показывать по </p>
@@ -43,14 +80,16 @@ export default function ListRoutes() {
           <span>20</span>
         </div>
       </header>
+
       <main className='main-list-routes'>
         <TrainRoute/>
       </main>
+
       <footer className='footer-list-routes'>
         <div className='list-routes-pages'>
           <div className='list-routes-pages-previous'></div>
           {pages.map((el, i) => 
-            <div className='list-routes-page'>{i + 1}</div>
+            <div className='list-routes-page' onClick={choicePage} key={i}>{i + 1}</div>
           )}
           <div className='list-routes-pages-next'></div>
         </div>
--- src/components/TrainRoute.jsx
+++ src/components/TrainRoute.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from 'react';
+import '../styles/train-route.css';
 
 export default function TrainRoute() {
   return (
-    <div>TrainRoute</div>
+    <div className='train-route'>
+      <div className='train-name'>
+        <span className='train-name-image'></span>
+        <h4 className='train-name-text'>020У</h4>
+        <div className='train-name-direction'>
+          <p className='train-name-city'>москва&#8594;</p>
+          <p className='train-name-city'>Санкт-Петербург</p>
+          <p className='train-name-city'>&#171;Мегаполис&#187;</p>
+        </div>
+      </div>
+
+      <div className='train-direction'>
+        <div className='train-direction-route'>
+          <div className='train-direction-from'>
+            <div className='direction-time'>00:20</div>
+            <div className='direction-from'>
+              <h5 className='direction-city'>Москва</h5>
+              <p className='direction-station'>Ленинградский</p>
+              <p className='direction-station'>вокзал</p>
+            </div>
+          </div>
+          <div className='train-direction-time'>
+            <p className='travel-time'>8:39</p>
+            <span className='direction-arrow'></span>
+          </div>
+          <div className='train-direction-to'>
+            <div className='direction-time'>08:59</div>
+            <div className='direction-to'>
+              <h5 className='direction-city'>Санкт-Петербург</h5>
+              <p className='direction-station'>Московский</p>
+              <p className='direction-station'>вокзал</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='train-tickets'>
+        <div className='train-tickets-options'>
+          <div className='train-ticket'>
+            <p className='ticket-class'>Купе</p>
+            <span className='amount-seat'>90</span>
+            <div className='ticket-start-price'>
+              <p className='ticket-start-text'>от</p>
+              <p className='ticket-start-number'>3500</p>
+              <span className='sign-rub'></span>
+            </div>
+          </div>
+          <div className='train-ticket'>
+            <p className='ticket-class'>Люкс</p>
+            <span className='amount-seat'>32</span>
+            <div className='ticket-start-price'>
+              <p className='ticket-start-text'>от</p>
+              <p className='ticket-start-number'>4500</p>
+              <span className='sign-rub'></span>
+            </div>
+          </div>
+        </div>
+        <div className='train-facilities'>
+          <span className='train-facilities-wifi'></span>
+        </div>
+        <button type='button' className='train-choice-btn'>Выбрать места</button>
+      </div>
+    </div>
   )
 }
