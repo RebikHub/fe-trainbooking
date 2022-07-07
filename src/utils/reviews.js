dfe3a9eb281b29667a14cd48511930bef25a031f
RebikHub <rebikovlk@mail.ru>
2022-07-07 17:05:21 +0700

    image fix

--- src/utils/reviews.js
+++ src/utils/reviews.js
@@ -1,32 +1,35 @@
+import avaw from '../images/avaw.png';
+import avam from '../images/avam.png';
+
 export const reviews = [
   {
     name: 'Екатерина Вальнова',
     content: 'Доброжелательные подсказки на всех этапах помогут правильно заполнить поля и без затруднений купить авиа или ж/д билет, даже если вы заказываете онлайн билет впервые.',
-    image: 'https://s3-alpha-sig.figma.com/img/b9b8/b89d/2fec5f0f798b8787930c80dd5d6472af?Expires=1655078400&Signature=bBctXxklMFyeibycUcu1ysZ9hzOk2owzOpp7RFmqT5xoG4xx-gykxuIGYV2xYIrNmU0mGbj5NOUm4fd2Ws08kxpNBrPLhQhQPZe31CYVqXAes1atk3hJGaGAZxLaNHiKZugPq4TSXkYhyr3G5~Bd8OVTDUrwimq0ACzyywoje0KNPWx9OR1WNwZa4vzOW1uTQF8mlZJkya5MOvHgqNcMCLI5abVItlyGN8tSJfQNiSao9-0oE2CyaMmnoAJzYJE1elelFOS2WdthO5pd2Dl9Utc6W5~2e6b6euoU8uX2DKDeRb0~OUmokeYUYjvYQsyTyI4ME-Z4jcA4-F4IPnHl6Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    image: avaw
   },
   {
     name: 'Евгений Стрыкало',
     content: 'СМС-сопровождение до посадки Сразу после оплаты ж/д билетов и за 3 часа до отправления мы пришлем вам СМС-напоминание о поездке.',
-    image: 'https://s3-alpha-sig.figma.com/img/9a94/671a/adb42ec0d354460adb5d422bc34dc958?Expires=1655078400&Signature=gYGum3PhH0MuruddX9t0vGZHnQ~1t5rbSW8Dkyu85BEGV-iG4qmnn-rrtp7dC-63loWPLI9gamRQ32fa1nVO5HYvve78UyknQF-UqXwd~NXUaNmR2DG7QgdQ~3JgHPfB9otjS8eODvPItc3~j-SitzUJO1C-g91ptkSO2XkdkCcs~mnU0QQlT5ZNPo6kyXapuQBYZ2L0arG3Pqn~d9GQTkHDWyyJEuaLgv04wujmYo07yjgE~j~wRVXr5pgYNORjepzAwUXXBsuzjgvIM99lLxmV6m9OzaRBNL57qenP3Lb-Uj6Y-OhB1qG4SJazsvTkk4XAVDtvrl6xqUY87AthWQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    image: avam
   },
   {
     name: 'Екатерина Вальнова',
     content: 'Доброжелательные подсказки на всех этапах помогут правильно заполнить поля и без затруднений купить авиа или ж/д билет, даже если вы заказываете онлайн билет впервые.',
-    image: 'https://s3-alpha-sig.figma.com/img/b9b8/b89d/2fec5f0f798b8787930c80dd5d6472af?Expires=1655078400&Signature=bBctXxklMFyeibycUcu1ysZ9hzOk2owzOpp7RFmqT5xoG4xx-gykxuIGYV2xYIrNmU0mGbj5NOUm4fd2Ws08kxpNBrPLhQhQPZe31CYVqXAes1atk3hJGaGAZxLaNHiKZugPq4TSXkYhyr3G5~Bd8OVTDUrwimq0ACzyywoje0KNPWx9OR1WNwZa4vzOW1uTQF8mlZJkya5MOvHgqNcMCLI5abVItlyGN8tSJfQNiSao9-0oE2CyaMmnoAJzYJE1elelFOS2WdthO5pd2Dl9Utc6W5~2e6b6euoU8uX2DKDeRb0~OUmokeYUYjvYQsyTyI4ME-Z4jcA4-F4IPnHl6Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    image: avaw
   },
   {
     name: 'Евгений Стрыкало',
     content: 'СМС-сопровождение до посадки Сразу после оплаты ж/д билетов и за 3 часа до отправления мы пришлем вам СМС-напоминание о поездке.',
-    image: 'https://s3-alpha-sig.figma.com/img/9a94/671a/adb42ec0d354460adb5d422bc34dc958?Expires=1655078400&Signature=gYGum3PhH0MuruddX9t0vGZHnQ~1t5rbSW8Dkyu85BEGV-iG4qmnn-rrtp7dC-63loWPLI9gamRQ32fa1nVO5HYvve78UyknQF-UqXwd~NXUaNmR2DG7QgdQ~3JgHPfB9otjS8eODvPItc3~j-SitzUJO1C-g91ptkSO2XkdkCcs~mnU0QQlT5ZNPo6kyXapuQBYZ2L0arG3Pqn~d9GQTkHDWyyJEuaLgv04wujmYo07yjgE~j~wRVXr5pgYNORjepzAwUXXBsuzjgvIM99lLxmV6m9OzaRBNL57qenP3Lb-Uj6Y-OhB1qG4SJazsvTkk4XAVDtvrl6xqUY87AthWQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    image: avam
   },
   {
     name: 'Екатерина Вальнова',
     content: 'Доброжелательные подсказки на всех этапах помогут правильно заполнить поля и без затруднений купить авиа или ж/д билет, даже если вы заказываете онлайн билет впервые.',
-    image: 'https://s3-alpha-sig.figma.com/img/b9b8/b89d/2fec5f0f798b8787930c80dd5d6472af?Expires=1655078400&Signature=bBctXxklMFyeibycUcu1ysZ9hzOk2owzOpp7RFmqT5xoG4xx-gykxuIGYV2xYIrNmU0mGbj5NOUm4fd2Ws08kxpNBrPLhQhQPZe31CYVqXAes1atk3hJGaGAZxLaNHiKZugPq4TSXkYhyr3G5~Bd8OVTDUrwimq0ACzyywoje0KNPWx9OR1WNwZa4vzOW1uTQF8mlZJkya5MOvHgqNcMCLI5abVItlyGN8tSJfQNiSao9-0oE2CyaMmnoAJzYJE1elelFOS2WdthO5pd2Dl9Utc6W5~2e6b6euoU8uX2DKDeRb0~OUmokeYUYjvYQsyTyI4ME-Z4jcA4-F4IPnHl6Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    image: avaw
   },
   {
     name: 'Евгений Стрыкало',
     content: 'СМС-сопровождение до посадки Сразу после оплаты ж/д билетов и за 3 часа до отправления мы пришлем вам СМС-напоминание о поездке.',
-    image: 'https://s3-alpha-sig.figma.com/img/9a94/671a/adb42ec0d354460adb5d422bc34dc958?Expires=1655078400&Signature=gYGum3PhH0MuruddX9t0vGZHnQ~1t5rbSW8Dkyu85BEGV-iG4qmnn-rrtp7dC-63loWPLI9gamRQ32fa1nVO5HYvve78UyknQF-UqXwd~NXUaNmR2DG7QgdQ~3JgHPfB9otjS8eODvPItc3~j-SitzUJO1C-g91ptkSO2XkdkCcs~mnU0QQlT5ZNPo6kyXapuQBYZ2L0arG3Pqn~d9GQTkHDWyyJEuaLgv04wujmYo07yjgE~j~wRVXr5pgYNORjepzAwUXXBsuzjgvIM99lLxmV6m9OzaRBNL57qenP3Lb-Uj6Y-OhB1qG4SJazsvTkk4XAVDtvrl6xqUY87AthWQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'
+    image: avam
   },
 ];
