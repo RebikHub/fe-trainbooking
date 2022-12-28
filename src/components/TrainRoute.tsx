86f4b99d030582bee12dfeed190d7b16cd572cf8
RebikHub <rebikovlk@mail.ru>
2022-12-28 16:46:54 +0200

    TrainRoute props route - null

--- src/components/TrainRoute.tsx
+++ src/components/TrainRoute.tsx
@@ -12,8 +12,8 @@ import { dateFromAndTo, duration } from '../utils/trainDate';
 import TrainRouteSeats from './TrainRouteSeats';
 
 type Props = {
-  route: IItem,
-  btnText: string
+  route: IItem | null,
+  btnText?: string
 };
 
 export default function TrainRoute({route, btnText = 'Выбрать места'}: Props) {
@@ -24,30 +24,34 @@ export default function TrainRoute({route, btnText = 'Выбрать места'
   useEffect(() => {
     const arrayInfo: TrainSeatsInfo[] = [];
 
-    if (route.departure.have_first_class) {
-      createArray(route, arrayInfo, 'first', 'Люкс');
+    if (route) {
+      if (route.departure.have_first_class) {
+        createArray(route, arrayInfo, 'first', 'Люкс');
+      };
+  
+      if (route.departure.have_second_class) {
+        createArray(route, arrayInfo, 'second', 'Купе');
+      };
+  
+      if (route.departure.have_third_class) {
+        createArray(route, arrayInfo, 'third', 'Плацкарт');
+      };
+  
+      if (route.departure.have_fourth_class) {
+        createArray(route, arrayInfo, 'fourth', 'Сидячий');
+      };
+  
+      setTrain(arrayInfo);
     };
-
-    if (route.departure.have_second_class) {
-      createArray(route, arrayInfo, 'second', 'Купе');
-    };
-
-    if (route.departure.have_third_class) {
-      createArray(route, arrayInfo, 'third', 'Плацкарт');
-    };
-
-    if (route.departure.have_fourth_class) {
-      createArray(route, arrayInfo, 'fourth', 'Сидячий');
-    };
-
-    setTrain(arrayInfo);
   }, [route]);
 
   function getCoaches() {
-    dispatch(choiceRoute(route));
-    dispatch(getSeatsThunk(route.departure._id));
-    dispatch(addRouteId(route.departure._id));
-    navigate('/route/coach');
+    if (route) {
+      dispatch(choiceRoute(route));
+      dispatch(getSeatsThunk(route.departure._id));
+      dispatch(addRouteId(route.departure._id));
+      navigate('/route/coach');
+    };
   };
 
   function backOrder() {
@@ -59,33 +63,33 @@ export default function TrainRoute({route, btnText = 'Выбрать места'
     <div className='train-route'>
       <div className='train-name'>
         <span className='train-name-image'></span>
-        <h4 className='train-name-text'>{route.departure.train._id}</h4>
+        <h4 className='train-name-text'>{route?.departure.train._id}</h4>
         <div className='train-name-direction'>
-          <p className='train-name-city'>{route.departure.from.city.name}&#8594;</p>
-          <p className='train-name-city'>{route.departure.to.city.name}</p>
-          <p className='train-name-city'>&#171;{route.departure.train.name}&#187;</p>
+          <p className='train-name-city'>{route?.departure.from.city.name}&#8594;</p>
+          <p className='train-name-city'>{route?.departure.to.city.name}</p>
+          <p className='train-name-city'>&#171;{route?.departure.train.name}&#187;</p>
         </div>
       </div>
 
       <div className='train-direction'>
         <div className='train-direction-route'>
           <div className='train-direction-from'>
-            <div className='direction-time'>{dateFromAndTo(route.departure.from.datetime)}</div>
+            <div className='direction-time'>{dateFromAndTo(route?.departure.from.datetime)}</div>
             <div className='direction-from'>
-              <h5 className='direction-city'>{route.departure.from.city.name}</h5>
-              <p className='direction-station'>{route.departure.from.city._id}</p>
+              <h5 className='direction-city'>{route?.departure.from.city.name}</h5>
+              <p className='direction-station'>{route?.departure.from.city._id}</p>
               <p className='direction-station'>вокзал</p>
             </div>
           </div>
           <div className='train-direction-time'>
-            <p className='travel-time'>{duration(route.departure.duration)}</p>
+            <p className='travel-time'>{duration(route?.departure.duration)}</p>
             <span className='direction-arrow'></span>
           </div>
           <div className='train-direction-to'>
-            <div className='direction-time'>{dateFromAndTo(route.departure.to.datetime)}</div>
+            <div className='direction-time'>{dateFromAndTo(route?.departure.to.datetime)}</div>
             <div className='direction-to'>
-              <h5 className='direction-city'>{route.departure.to.city.name}</h5>
-              <p className='direction-station'>{route.departure.to.city._id}</p>
+              <h5 className='direction-city'>{route?.departure.to.city.name}</h5>
+              <p className='direction-station'>{route?.departure.to.city._id}</p>
               <p className='direction-station'>вокзал</p>
             </div>
           </div>
@@ -106,9 +110,9 @@ export default function TrainRoute({route, btnText = 'Выбрать места'
         </div>
 
         <div className='train-facilities'>
-          <span className={`${route.departure.have_wifi ? 'facilities-wifi-have' : 'train-facilities-wifi'}`}></span>
-          <span className={`${route.departure.is_express ? 'facilities-express-have' : 'train-facilities-express'}`}></span>
-          <span className={`${route.departure.have_air_conditioning ? 'facilities-coffee-have' : 'train-facilities-coffee'}`}></span>
+          <span className={`${route?.departure.have_wifi ? 'facilities-wifi-have' : 'train-facilities-wifi'}`}></span>
+          <span className={`${route?.departure.is_express ? 'facilities-express-have' : 'train-facilities-express'}`}></span>
+          <span className={`${route?.departure.have_air_conditioning ? 'facilities-coffee-have' : 'train-facilities-coffee'}`}></span>
         </div>
 
         {btnText !== 'Изменить' ?
